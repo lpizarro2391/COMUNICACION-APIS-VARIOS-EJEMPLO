f6854106a2585f8ca39cb4682631312460184f04
lpizarro <lpizarro2391@github.com>
2020-05-26 19:52:41 -0600

    creacion variables personlas

--- estructura_compleja.json/estructura_compleja.js
+++ estructura_compleja.json/estructura_compleja.js
@@ -1,13 +1,40 @@
 var Estructura= {
 "Personas": [
-    {
-        "Nombre": "Mateo",
-        "Edad": 24,
-        "Direccion"
-    }
-]
+        {
+            "Nombre": "Mateo",
+            "Edad": 24,
+            "Direccion": "Calle 108# 66-28",
+            "Telefono": "(857) 444-44-44",
+            "Estudios": [
+                "Primaria Escolar":{
+                    "lugar": "Universidad Pontificia Bolivariano",
+                    "Fecha":"2006-2005"
+                },
+                "Secundaria Escolar": {
+                    "Lugar": "Universidad Pontificia Bolivariano",
+                    "Fecha": "2006-2005"
+                }
+            ]
+        },
+        {
+            "Nombre": "Guillermo",
+            "Edad": 30,
+            "Direccion": "Calle 98# 21-48",
+            "Telefono": "(857) 233-32-32",
+            "Estudios": [
+                "Primaria Escolar": {
+                    "lugar": "Concejo de Medellin",
+                    "Fecha": "1995-2000"
+                },
+                "Secundaria Escolar": {
+                    "Lugar": "Concejo de Medellin",
+                    "Fecha": "2000-2011"
+                }
+            ]
+        }
 
-
-
-
+    ]
 }
+
+var persona1 = Estructura.personas[0];
+var persona2 = Estructura.personas[1];
