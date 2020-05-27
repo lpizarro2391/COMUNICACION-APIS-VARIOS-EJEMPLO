32a3be54a41aeb135c3fad6e68b4d49db122725e
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-27 10:52:55 -0600

    agregar funcion de localizacion en el mapa, asignaremos un evento en el boton y el la posicion que ubique el mapa, y al elemento info window que nos diga ubicacion encontrada y se situe en la ubicacion

--- geolocalizacion/index.js
+++ geolocalizacion/index.js
@@ -11,3 +11,13 @@ if(navigator.geolocation){
 }else{
     alert('tu navegador no soportala geolocalizacion')
 }
+
+function initMap(){
+   var mapContainer = document.getElementById('map')
+   var config = {
+       center:{lat:-34.37,long:150.644 },
+       zoom: 5
+   }
+   map = new google.com.maps.Map(mapContainer,config)
+   infoWindow = new google.maps.infoWindow({map: map})
+}
