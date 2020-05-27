aa56b63e9c66023e220dde76b637e269fc7edbb1
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-27 16:47:26 -0600

    render site, objeto 1. agregar la info visual a la agenda que se abre , utiliza html como cadena de texto, luego se cambian por los valores realies e insertados en el dom.

--- AlmacenamientoBase_localizacion/index.js
+++ AlmacenamientoBase_localizacion/index.js
@@ -28,7 +28,7 @@ function initMap() {
   }
   map = new google.maps.Map(mapContainer, config)
   infoWindow = new google.maps.InfoWindow({ map: map })
-
+  Agenda.init()
 }
 
 var button = document.getElementById('btn-geo')
@@ -82,6 +82,20 @@ saveAndPlaceMarker: function(site){
   this.renderSite(site)
 },
 
+renderSite: function(site){
+  var htmlInfo = '<li class="collection-item avatar">'+
+  '<i class = "material-icons cicle blue">thumb_up</i>'+
+  '<span class="title"> : nombre: </span>'+
+  '<p> Latitud: : latitud: <br> longitud: :longitud: <br> Descripcion: : descripcion:</p>'+
+  '<a href="#!" class="secondary-content"> <i class="material-icons">grade</i></a>'+
+  '</li>';  
+  
+
+
+
+
+
+}
 
 
 
