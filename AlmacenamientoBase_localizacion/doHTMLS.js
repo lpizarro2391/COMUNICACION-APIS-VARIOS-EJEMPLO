de4a96430dabf69458dc0fe229838dd22cb8b60d
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-28 16:52:10 -0600

    crear el escuchador de eventos y meter la funcionalidad en el callback del archivo doHTML usando edata., asignar los datos de nuetro sitio a una variable y rresponder con un resultado

--- AlmacenamientoBase_localizacion/doHTMLS.js
+++ AlmacenamientoBase_localizacion/doHTMLS.js
@@ -1,12 +1,16 @@
-var htmlInfo = '<li class="collection-item avatar">' +
-    '<i class = "material-icons cicle blue">thumb_up</i>' +
-    '<span class="title"> :nombre: </span>' +
-    '<p> Latitud: :latitud: <br> longitud: :longitud: <br> Descripcion: :descripcion:</p>' +
-    '<a href="#!" class="secondary-content"> <i class="material-icons">grade</i></a>' +
-    '</li>';
+self.addEventListener("message",function(e){
+    var site= e.data
+    var htmlInfo = '<li class="collection-item avatar">' +
+        '<i class = "material-icons cicle blue">thumb_up</i>' +
+        '<span class="title"> :nombre: </span>' +
+        '<p> Latitud: :latitud: <br> longitud: :longitud: <br> Descripcion: :descripcion:</p>' +
+        '<a href="#!" class="secondary-content"> <i class="material-icons">grade</i></a>' +
+        '</li>';
 
-var newSite = htmlInfo
-var result = newSite.replace(':nombre:', site.nombre)
-    .replace(':latitud:', site.latitud)
-    .replace(':longitud:', site.longitud)
-    .replace(':descripcion:', site.descripcion)
+    var newSite = htmlInfo
+    var result = newSite.replace(':nombre:', site.nombre)
+        .replace(':latitud:', site.latitud)
+        .replace(':longitud:', site.longitud)
+        .replace(':descripcion:', site.descripcion)
+    self.postMessage(result)
+})
