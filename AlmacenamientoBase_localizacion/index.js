eb6d336ad97419c7db101130e8d3195df9061bba
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-28 15:23:44 -0600

    correccion de espacios entre puntos de replace

--- AlmacenamientoBase_localizacion/index.js
+++ AlmacenamientoBase_localizacion/index.js
@@ -85,8 +85,8 @@ saveAndPlaceMarker: function(site){
 renderSite: function(site){
   var htmlInfo = '<li class="collection-item avatar">'+
   '<i class = "material-icons cicle blue">thumb_up</i>'+
-  '<span class="title"> : nombre: </span>'+
-  '<p> Latitud: : latitud: <br> longitud: :longitud: <br> Descripcion: : descripcion:</p>'+
+  '<span class="title"> :nombre: </span>'+
+  '<p> Latitud: :latitud: <br> longitud: :longitud: <br> Descripcion: :descripcion:</p>'+
   '<a href="#!" class="secondary-content"> <i class="material-icons">grade</i></a>'+
   '</li>'; 
   
